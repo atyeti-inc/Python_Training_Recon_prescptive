4a501f5b2de5d88db77b347d6f5f8ddc77dc893c
Dhananjay Deshpande <dhananjay.deshpande@atyeti.com>
2025-09-25 12:05:36 +0530

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,28 @@
+#Write a Program to input 2 numbers & print their sum
+
+n1 = int(input("first number:"))
+n2 = int(input("second number:"))
+sum= n1+n2
+print ("The sum of two numbers is:",sum)
+
+#Write a Program to input side of a square & print its area
+
+n1 = int(input("Side of a Square to find Area:"))
+Area= n1*n1
+print ("The Area of Square is:",Area)
+
+#Write a Program to input 2 floating point numbers & print their average.
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+avgerage = (num1+num2)/2
+print("The average of two numbers is",avgerage)
+
+#Write a Program to input 2 int numbers, a and b.Print True if a is greater than or equal to b. If not print False.
+
+n1 = int(input("Enter first number 'a':"))
+n2 = int(input("Enter Second number 'b':"))
+if n1>= n2:
+   print ("True")
+else:
+   print("False")
