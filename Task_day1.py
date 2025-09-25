4ced9d4a573ba66d61b27bba0bedea3460922016
Chunchu sandeep <sandeep.chunchu@atyeti.com>
2025-09-25 16:32:24 +0530

    Create Task_day1.py

---
+++ Task_day1.py
@@ -0,0 +1,37 @@
+
+#practice 
+
+#write a program to input 2 numbers and print their sum
+
+num1=int(input("Num1: "))
+num2=int(input("Num2: "))
+sum=num1+num2
+print(sum)
+
+# WAP to input side of a square & print its area.
+
+
+side=int(input("side :"))
+area=side * side
+print(area)
+
+
+
+
+# WAp to input 2 floating point numbers and print their average
+
+
+f1=float(input("Num1 : "))
+f2=float(input("Num2 : "))
+
+average=(f1+f2)/2
+print(average)
+
+#WAP to input 2 int numbers, a and b.Print True if a is greater than or equal to b. If not print False.
+
+a=int(input("A value : "))
+b=int(input("B value : "))
+if(a>=b):
+    print(True)
+else:
+    print(False) 
