f5cb11636fc5bec04729c8c86e25422b966411d7
Mk1281 <manish.upadhyay@atyeti.com>
2025-09-25 10:19:53 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,37 @@
+Assignment 1:
+
+------------------------------------------------------
+N1 = int(input("Enter first number:"))
+N2 = int(input("Enter second number:"))
+sum= N1+N2
+print ("The sum of the two Numbers is:",sum)
+------------------------------------------------------
+
+------------------------------------------------------
+
+N1 = int(input("Enter the side of a Square to find Area:"))
+
+Area= N1*N1
+print ("The Area of Square is:",Area)
+
+------------------------------------------------------
+
+------------------------------------------------------
+
+N1 = float(input("Enter first Floating number:"))
+N2 = float(input("Enter second Floating number:"))
+Avg= (N1+N2)/2
+print ("The Average of the two Floating Numbers is:",Avg)
+
+------------------------------------------------------
+
+------------------------------------------------------
+
+N1 = int(input("Enter first number 'a':"))
+N2 = int(input("Enter Second number 'b':"))
+if N1>= N2:
+   print ("True")
+else:
+   print("False")
+
+------------------------------------------------------
