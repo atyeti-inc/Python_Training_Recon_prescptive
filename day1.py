e03c901987453c84f085800da80a8dc920169ea4
Reepud <Reepu.Daman@atyeti.com>
2025-09-25 11:11:45 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,18 @@
+x = int (input("Enter a number :"))
+y = int (input("Enter second number:"))
+sum = x+y
+print ("Sum of the numbers is : "+ str(sum))
+
+x = int (input("Side of square:"))
+sum = x**2
+print ("Area of the square ="+str(sum))
+
+x = float (input("Enter a number :"))
+y = float (input("Enter second number:"))
+avg = float ((x+y)/2)
+print ("The average of the numbers is : " + str(avg))
+
+x = int (input("Enter a number a :"))
+y = int (input("Enter second number b :"))
+msg = "True" if x>=y else "False"
+print (msg)
