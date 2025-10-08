709c8afbaa568057d918fd61ac755b935b217b2a
rajeshwari1019 <rajeshwari.v@atyeti.com>
2025-10-08 16:01:49 +0530

    Task1_Completed

---
+++ Task_day1.py
@@ -0,0 +1,24 @@
+#Program to calculate the sum of two numbers -input from user
+a = int(input("Enter a  1st number: "))
+b = int(input("Enter a 2nd number: "))
+sum= a + b
+print("The sum of the two numbers is: " ,sum)
+
+#find area of a square input from use
+side=int(input("Enter the side of the square:"))
+area=side*side
+print("The area of the square is : ",area)
+
+# print the average of two floating point numbers take as input
+num1=float(input("Enter 1st number: "))
+num2=float(input("Enter 2nd number: "))
+avg=(num1+num2)/2
+print("The average of two numbers is: ",avg)
+
+# Find a greater number if a is greater than b retuen true else false
+x=int(input("Enter 1st number: "))
+y=int(input("Enter 2nd number: "))
+if x>=y:
+    print("True")
+else:
+    print("False")
